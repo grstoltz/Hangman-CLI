e48370db0aea7531002601f550c223579966c6e2
Grant Stoltz <grant@grantstoltz.com>
2018-01-25 00:04:41 -0700

    updating readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Hangman-CLI
+# Hangman CLI
+
+This is a hangman game built using Node.js
+
+## Getting Started
+
+After installing, run the app by typing the following into your console:
+
+```
+node cli.js
+```
+
+## Built With
+
+* [Random-Words](https://www.npmjs.com/package/random-words)
+* [Inquirer](https://www.npmjs.com/package/inquirer) 
--- cli.js
+++ cli.js
@@ -1,52 +1,52 @@
-var init = function (){
+var init = function () {
 
-var App = require("./app.js");
-var inquirer = require("inquirer");
+    var App = require("./app.js");
+    var inquirer = require("inquirer");
 
-var game = new App();
+    var game = new App();
 
-game.displayBoard();
+    game.displayBoard();
 
-var playGame = function () {
+    var playGame = function () {
 
-    if (game.state === "active") {
+        if (game.state === "active") {
 
-        inquirer.prompt([
-            {
-                type: "input",
-                message: "What is your guess?",
-                name: "guess"
-            }
-        ]).then(function (response) {
-            var guess = response.guess
-            game.userGuess(guess);
-            game.displayBoard();
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What is your guess?",
+                    name: "guess"
+                }
+            ]).then(function (response) {
+                var guess = response.guess
+                game.userGuess(guess);
+                game.displayBoard();
 
-            playGame();
-        })
+                playGame();
+            })
+        }
+        else if (game.state === "won" || game.state === "lost") {
+            inquirer.prompt([
+                {
+                    type: "confirm",
+                    message: "Would you like to play again?",
+                    name: "response"
+                }
+            ]).then(function (user) {
+
+                if (user.response) {
+
+                    main();
+                }
+                else {
+                    game.endGame()
+                }
+
+            })
+        }
     }
-    else if (game.state === "won" || game.state === "lost") {
-        inquirer.prompt([
-            {
-                type: "confirm",
-                message: "Would you like to play again?",
-                name: "response"
-            }
-        ]).then(function (user) {
 
-            if (user.response) {
-                
-                main();
-            }
-            else {
-                game.endGame()
-            }
-
-        })
-    }
-}
-
-playGame();
+    playGame();
 
 }
 
